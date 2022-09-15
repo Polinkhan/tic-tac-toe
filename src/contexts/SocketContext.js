5e93a82befad599a01400fcf2de2f2dfe001bbf9
Polinkhan <polinsir@gmail.com>
2022-09-15 13:40:24 +0600

    first commit

--- src/contexts/SocketContext.js
+++ src/contexts/SocketContext.js
@@ -4,8 +4,8 @@ const SocketContext = createContext();
 export const useSocketContext = () => useContext(SocketContext);
 
 function SocketContextProvoder(props) {
-  //   const socket = io.connect("https://superchat-socket-server.herokuapp.com");
-  const socket = io.connect("https://games-01.herokuapp.com");
+  // const socket = io.connect("https://games-01.herokuapp.com");
+  const socket = io.connect("localhost:8000");
   const value = { socket };
 
   return <SocketContext.Provider value={value}>{props.children}</SocketContext.Provider>;
